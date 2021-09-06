c99b930e7dc04793e5a064e575262732050d3126
esir <esirkings@gmail.com>
2021-09-06 04:48:06 +0300

    Update Referer to the URL for which we're fetching the stats

--- stats_scrapper.py
+++ stats_scrapper.py
@@ -6,7 +6,6 @@ import urllib.request
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
     "Accept-Language": "en-US,en;q=0.5",
-    "Referer": "https://mediacloud.org/"
 }
 
 def generate_facebook_stats_url(url, width=35, height=65):
@@ -17,14 +16,15 @@ def generate_facebook_stats_url(url, width=35, height=65):
     stats_url = f"""https://en-gb.facebook.com/plugins/like.php?href={url}&width={width}&layout=box_count&action=like&size=small&share=false&height={height}&appId"""
     return stats_url
 
-def generate_iframe_soup(likes_url):
+def generate_iframe_soup(likes_url, referer_url):
+    headers["Referer"] = referer_url
     request = urllib.request.Request(likes_url, headers=headers)
     response = urllib.request.urlopen(request)
     iframe_soup = BeautifulSoup(response, "html.parser")
     return iframe_soup
 
-def extract_facebook_stats(stats_url):
-    iframe_soup = generate_iframe_soup(stats_url)
+def extract_facebook_stats(stats_url, referer_url):
+    iframe_soup = generate_iframe_soup(stats_url, referer_url)
     stats_count = iframe_soup.select_one('table td .inlineBlock span').text
     if stats_count.endswith("K"): # e.g 1.2K => one thousand two hundred. We require another 100 in order to get to 1.3K so we can say the accuracy is 100 
         accuracy = 100
--- test_stats_scrapper.py
+++ test_stats_scrapper.py
@@ -3,5 +3,5 @@ from stats_scrapper import generate_facebook_stats_url, extract_facebook_stats
 with open('urls.txt') as f:
     for url in f.readlines():
         stats_url = generate_facebook_stats_url(url)
-        likes_shares_comments_count = extract_facebook_stats(stats_url)
+        likes_shares_comments_count = extract_facebook_stats(stats_url, url)
         print(f"{url.strip()} - {likes_shares_comments_count}")
